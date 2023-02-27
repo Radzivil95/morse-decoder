48b212cc6a20b424b37be7539bc29e34d96e37d8
VladRadzivil <vladradziviloff@gmail.com>
2023-02-27 13:06:33 +0100

    feat: complite

--- src/index.js
+++ src/index.js
@@ -38,8 +38,39 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let arr = [];
+    for (let i = 0; i < expr.length; i+= 10) {
+        arr.push(expr.slice(i, i + 10));
+    }
+
+    let morse = [];
+    for(let i = 0; i < arr.length; i++) {
+        let x = '';
+        for(let j = 0; j < arr[i].length; j+=2) {
+            if(arr[i][j] + arr[i][j+1] == 10) {
+                x += '.';
+            }
+            if(arr[i][j] + arr[i][j+1] == 11) {
+                x += "-";
+            }
+            if(arr[i][j] + arr[i][j+1] == '**') {
+                x = '**';
+            }
+        }
+        morse.push(x);
+    }
+    let x = '';
+    for (let i=0;i< morse.length;i++){
+        if(morse[i] == '**') {
+            x += ' ';
+        } else {
+            x += MORSE_TABLE[morse[i]]
+        }
+    }
+    return x;
+
 }
+
 
 module.exports = {
     decode
